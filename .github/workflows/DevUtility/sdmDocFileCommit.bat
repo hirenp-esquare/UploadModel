7f50c69251387660451474a2483febaa66fb3900
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-12-02 10:50:42 +0530

    Update sdmDocFileCommit.bat

--- .github/workflows/DevUtility/sdmDocFileCommit.bat
+++ .github/workflows/DevUtility/sdmDocFileCommit.bat
@@ -1,4 +1,12 @@
 @echo off
 set filePath= %1
+set workspacePath= %2
 set filePath=%filePath:.mdl=-A.html%
 echo %filePath%
+
+Rem remove double quotes using replacing 
+set filePath=%filePath:"=%   
+setlocal ENABLEDELAYEDEXPANSION
+set ipList=
+for %%i in (%filePath%) do ( set ipList=!ipList!%workspacePath%%%i )
+echo %ipList%
