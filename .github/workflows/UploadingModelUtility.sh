5190cfce33824444d9dcd0b5adc8089c67af38a6
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2021-01-08 15:12:27 +0530

    Update UploadingModelUtility.sh

--- .github/workflows/UploadingModelUtility.sh
+++ .github/workflows/UploadingModelUtility.sh
@@ -39,12 +39,12 @@ cd "$CLONE_DIR"
 
 echo "Rename file for forio model"
 cd 
-mv $INPUT_DESTINATION_FOLDER/[cc_]*.vmfx $INPUT_DESTINATION_FOLDER/CC_PROD.vmfx
+mv "${INPUT_DESTINATION_FOLDER}"/[cc_]*.vmfx "${INPUT_DESTINATION_FOLDER}/CC_PROD.vmfx"
 #mv [mm_]*.vmfx MM_PROD.vmfx
-mv [psy_]*.vmfx Psy_PROD.vmfx
+#mv [psy_]*.vmfx Psy_PROD.vmfx
 #mv [agg_]*.vmfx Agg_PROD.vmfx
 mv [sp_]*.vmfx SuicidePrevention_PROD.vmfx
-cd ..
+#cd ..
 #End rename file for forio model
 
 
