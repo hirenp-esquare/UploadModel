c62ffee7a3fbb7650ef97e7bcb1fa60fe4790082
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2021-06-14 14:49:12 +0530

    Update UploadingModelUtility.sh

--- .github/workflows/UploadingModelUtility.sh
+++ .github/workflows/UploadingModelUtility.sh
@@ -24,9 +24,8 @@ CLONE_DIR=$(mktemp -d)
 echo "Cloning destination git repository"
 git config --global user.email $INPUT_USER_EMAIL
 git config --global user.name $INPUT_USER_NAME
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+echo git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "$CLONE_DIR"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
@@ -35,9 +34,9 @@ then
 fi
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/INPUT_DESTINATION_FOLDER
-cp -R $INPUT_SOURCE_FILE "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-cd "$CLONE_DIR"
+echo mkdir -p $CLONE_DIR/INPUT_DESTINATION_FOLDER
+echo cp -R $INPUT_SOURCE_FILE "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+echo cd "$CLONE_DIR"
 
 # "Start Rename file for forio model"
 #cd "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
